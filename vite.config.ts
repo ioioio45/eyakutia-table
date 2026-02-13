7a0e9e450a484319f547ddf8c9c4688e32a6a942
ioioio45 <nyrgun01@gmail.com>
2026-02-13 16:49:34 +0900

    add vercel config, delete vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,4 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: './',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://edu.e-yakutia.ru',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false
-      }
-    }
-  }
 });
